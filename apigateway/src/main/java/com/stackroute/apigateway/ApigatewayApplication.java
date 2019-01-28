5ee0686b7cf6fed14f332da8153cb2e1ce9a88f6
urvashi.ratna <urvashi.ratna@boeing.com>
2019-01-28 17:24:24 +0530

    api gateway update

--- apigateway/src/main/java/com/stackroute/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/stackroute/apigateway/ApigatewayApplication.java
@@ -2,12 +2,36 @@ package com.stackroute.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import com.stackroute.apigateway.filters.ErrorFilter;
+import com.stackroute.apigateway.filters.PostFilter;
+import com.stackroute.apigateway.filters.PreFilter;
+import com.stackroute.apigateway.filters.RouteFilter;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
 	}
 
 }
