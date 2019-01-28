0893b18529d0fc52b81a7a3d716fe1acda705767
gauravgkp <gkpgauravsingh@gmail.com>
2019-01-28 16:04:47 +0530

    redefined getAllUser methode, changed mail to userMail and contact to userContact

--- userProfile/src/main/java/com/stackroute/userProfile/service/UserProfileService.java
+++ userProfile/src/main/java/com/stackroute/userProfile/service/UserProfileService.java
@@ -1,14 +1,15 @@
 package com.stackroute.userProfile.service;
 
 import java.util.List;
+import com.stackroute.userProfile.domain.UserProfile;
 
 public interface UserProfileService {
 
     public UserProfile saveTheUser(UserProfile userProfile) throws UserProfileAlreadyExistsException;
+    public List<UserProfile> getAllUser() throws UserProfileNotFoundExeption;
     public List<UserProfile> getUserByMail(String mail) throws UserMailNotFoundExeption;
     public List<UserProfile> getUserByContact(int contact) throws UserContactNotFoundExeption;
-    public List<UserProfile> getAllUser(UserProfile userProfile);
-    public UserProfile updateUser(String Mail,int contact) throws UserProfileNotFoundExeption;
+    public UserProfile updateUser(String mail,int contact) throws UserProfileNotFoundExeption;
     public List<UserProfile> deleteUserbyContact(int userContact) throws UserContactNotFoundException;
     public List<UserProfile> deleteUserbyMail(String userMail) throws UserMailNotFoundException;
 }
